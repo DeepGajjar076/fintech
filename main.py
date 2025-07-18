8ba5c2aab55f0baa0cbccbed0440b0afe602bab9
DeepGajjar076 <deepgajjar076@gmail.com>
2025-07-18 18:27:34 +0530

    main.py

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="FinTech", page_icon=":flag_in:", layout="wide" )
 
-def local_css(file_name):   # ignore
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("style/style.css")  # ignore
 
 st.subheader("Hi, I Am Deep :wave: ")
 st.title("Welcome To Fintech Studio")
@@ -38,7 +33,6 @@ contact_form = """
     <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Your name" required>
     <input type="email" name="email" placeholder="Your email" required>
-    <textarea name="message" placeholder="Your message here" required></textarea>
     <button type="submit">Send</button>
 </form>
 """
